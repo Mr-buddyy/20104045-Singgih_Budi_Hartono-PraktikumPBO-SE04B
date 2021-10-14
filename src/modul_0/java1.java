c73f1e14e34c48e8fd716311cdd4b1383f0dde1c
SinggihBudi <singgihbudi.sbh@gmail.com>
2021-10-14 14:38:14 +0700

    Create Praktikum PBO

--- src/modul_0/java1.java
+++ src/modul_0/java1.java
@@ -1,6 +1,9 @@
 package src.modul_0;
 public class java1{
     public static void main(String[] args) {
-        System.out.println(" hello world");
+        System.out.println("hello world");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello word-" + (i+1));
+        }
     }
 }
