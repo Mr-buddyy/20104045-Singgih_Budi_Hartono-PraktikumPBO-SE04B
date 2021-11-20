75decf55c76a4581b0ccaf17585bc9603e7c228f
Buddyy <20104045@ittelkom-pwt.ac.id>
2021-11-20 22:01:43 +0700

    modul_6

--- src/modul_6/praktikum/percobaan2/subbangun/Lingkaran.java
+++ src/modul_6/praktikum/percobaan2/subbangun/Lingkaran.java
@@ -6,10 +6,12 @@ public class Lingkaran extends BangunDatar {
     double jariJari;
 
     public double getJariJari() {
+
         return jariJari;
     }
 
     public void setJariJari(double jariJari) {
+
         this.jariJari = jariJari;
     }
 }
