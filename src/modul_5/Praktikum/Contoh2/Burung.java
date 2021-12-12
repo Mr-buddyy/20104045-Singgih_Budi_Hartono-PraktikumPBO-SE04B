6adcc2a05c6b4e67c0d843507ffc71921434015c
Buddyy <20104045@ittelkom-pwt.ac.id>
2021-12-13 04:18:40 +0700

    Praktikum

---
+++ src/modul_5/Praktikum/Contoh2/Burung.java
@@ -0,0 +1,9 @@
+package modul_5.Praktikum.Contoh2;
+
+public class Burung extends Hewan {
+    @Override
+    void bersuara() {
+        super.bersuara();
+        System.out.println("Pit.. pit...");
+    }
+}
